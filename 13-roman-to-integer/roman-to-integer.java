85318251ea68190d4171177bd17c560f58595bea
Devatva Rachit <127624471+Devatva24@users.noreply.github.com>
2025-09-28 20:13:57 +0530

    Time: 4 ms (69.83%) | Memory: 45.1 MB (22.93%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -8,8 +8,8 @@ class Solution {
         map.put('C', 100);
         map.put('D', 500);
         map.put('M', 1000);
-        int prev = 0;
         int sum = 0;
+        int prev = 0;
         for(int i=s.length()-1;i>=0;i--) {
             int curr = map.get(s.charAt(i));
             if(curr < prev) {
