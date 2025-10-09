79c86722fcb3afa29e96a5f6d534e0f898269e44
Devatva Rachit <127624471+Devatva24@users.noreply.github.com>
2025-10-09 20:19:08 +0530

    Time: 1 ms (94.46%) | Memory: 42.1 MB (27.22%) - LeetSync

---
+++ 520-detect-capital/detect-capital.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int count = 0;
+        for(int i=0;i<word.length();i++) {
+            char ch = word.charAt(i);
+            if(ch >= 'A' && ch <= 'Z') count++;
+        }
+        if(count == 0 || count == word.length()) return true;
+        if(count == 1 && (word.charAt(0) >= 'A' && word.charAt(0) <= 'Z')) return true;
+        return false;
+    }
+}
