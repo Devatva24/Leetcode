b3c10861c710f262d74087f6235ea59de8d987a6
Devatva Rachit <127624471+Devatva24@users.noreply.github.com>
2025-10-20 23:44:41 +0530

    Time: 1 ms (74.53%) | Memory: 42.8 MB (46.14%) - LeetSync

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.java
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.java
@@ -1,13 +1,8 @@
 class Solution {
     public int finalValueAfterOperations(String[] operations) {
-        String preIncrement = "++X";
-        String preDecrement = "--X";
-        String postIncrement = "X++";
-        String postDecrement = "X--";
         int sum = 0;
-        for(int i=0;i<operations.length;i++) {
-            if(operations[i].equals(postIncrement) || operations[i].equals(preIncrement)) sum+=1;
-            else sum-=1;
+        for(String op : operations) {
+            sum += (op.charAt(0) == '+' || op.charAt(op.length()-1) == '+') ? 1 : -1;
         }
         return sum;
     }
