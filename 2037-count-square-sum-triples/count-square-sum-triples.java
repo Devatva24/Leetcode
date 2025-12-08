ba5d527b6cec2fae69685fbcb1253ddc95b29702
Devatva Rachit <127624471+Devatva24@users.noreply.github.com>
2025-12-09 02:56:39 +0530

    Time: 17 ms (37.14%) | Memory: 46.1 MB (9.96%) - LeetSync

---
+++ 2037-count-square-sum-triples/count-square-sum-triples.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countTriples(int n) {
+        int count = 0;
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int i=1;i<=n;i++) {
+            set.add(i*i);
+        }
+        for(int i=1;i<n;i++) {
+            for(int j=i+1;j<=n;j++) {
+                int sum = (i * i) + (j * j);
+                if(set.contains(sum)) count++;
+            }
+        }
+        return count*2;
+    }
+}
